3e4aea3703739b9c07d3a0220de98538d89530c1
Shaun Wahle <shaun.wahle@gmail.com>
2017-09-28 18:02:56 -0700

    Added yesterdays code

---
+++ shaun-lab23/README.md
@@ -0,0 +1,3 @@
+Reddit Search App
+
+Using the power of React, I'm able to dynamically make api calls by passing the state of the inputs between props and into values. We also parsed through the object we received.
---
+++ shaun-lab23/src/components/note-create-form/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class NoteCreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      content: '',
+      editing: false,
+      completed: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleNoteCreate(this.state);
+  }
+
+  render() {
+    return (
+      <div className='note-create'>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name='title'
+          type='text'
+          placeholder='Title'
+          value={this.state.title}
+          onChange={this.handleChange}
+          />
+
+        <input
+          name='content'
+          type='text'
+          placeholder='Content'
+          value={this.state.content}
+          onChange={this.handleChange}
+          />
+
+        <button type='submit'>Add Note</button>
+      </form>
+    </div>
+    );
+  }
+}
+
+export default NoteCreateForm;
